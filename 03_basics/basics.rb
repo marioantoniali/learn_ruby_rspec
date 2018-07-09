c8ce9ab69578f2f6547fc3e32a347d4a8619abf5
Jihane Haddad <jihane@MacBook-Pro-de-Jihane.local>
2018-07-09 21:57:13 +0200

    Ajout exo 3 et 6

--- 03_basics/basics.rb
+++ 03_basics/basics.rb
@@ -1,1 +1,23 @@
-# write your code here
+def who_is_bigger(a, b, c)
+
+    return "nil detected" if a == nil || b == nil || c == nil
+    return "a is bigger" if a >= b && a >= c
+    return "b is bigger" if b >= a && b >= c
+    return "c is bigger" if c >= b && c >= b
+    
+end     
+
+
+def reverse_upcase_noLTA(string_to_update)    
+    string_to_update = string_to_update.downcase.delete "tla"
+    return string_to_update.reverse.upcase
+end
+
+def array_42(tab)
+    return tab.include? 42
+end    
+
+
+def magic_array(tab)    
+    return tab.flatten.uniq.sort.delete_if {|x| x%3 == 0 }.collect { |x| x * 2 } 
+end    
--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,36 @@
-#write your code here
+def translate(word)
+    
+    result  = []
+
+    word.split(" ").each{
+        |x|
+        
+        # on substitue les "qu" et les "sch" par des symboles
+        x = x.sub(/qu/, 'ç')  
+        x = x.sub(/sch/, '&')  
+        
+        i=0 
+        
+        #on bouche sur les lettres du mot pour trouver la 1er voyelle
+        while (! "aeiou".include? x[i].downcase) && i< x.size
+            i+=1
+        end
+    
+        motFinal = ""
+        
+        if i == 0
+            motFinal = "#{x[0..x.size]}ay"
+        elsif
+              motFinal = "#{x[i..x.size]}#{x[0..i-1]}ay"
+        end
+        
+        motFinal = motFinal.sub(/ç/, 'qu').sub(/&/, 'sch') 
+        
+        result.push motFinal
+    }
+    
+    return result.join(" ")
+    
+end
+
+
