e2ceb4f234d2140de2a603c68ea9e4d83fb420f9
FabriceS69 <fabrice.fullstack@gmail.com>
2018-07-09 21:06:05 +0200

    ajout fichiers 05

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,6 @@
-#write your code here
+def time_string(seconds)
+	hour = seconds / 3600
+	minute = ( seconds % 3600 ) / 60
+	sec = ( seconds % 3600) % 60
+	return '%02d:%02d:%02d' % [hour,minute,sec]
+end
